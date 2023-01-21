54ab4d494a7926ef8790e804e99db7ae58705c20
k.kosteniuk <konstantinkostenyuk@gmail.com>
2023-01-21 12:34:36 +0100

    Cash properties of findById and findAll have been added.

--- src/main/java/com/example/garage/repasitory/CarRepasitory.java
+++ src/main/java/com/example/garage/repasitory/CarRepasitory.java
@@ -6,9 +6,11 @@ package com.example.garage.repasitory;
 import com.example.garage.model.Car;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import org.yaml.snakeyaml.events.Event;
 
 import java.util.List;
 @Repository
 public interface CarRepasitory extends CrudRepository <Car, Integer>{
     List<Car> findAll();
+
 }
