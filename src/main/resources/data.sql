5376211744592bb823f4e075c58fbcb469edd088
k.kosteniuk <konstantinkostenyuk@gmail.com>
2023-01-24 12:36:40 +0100

    DEV works at all

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -16,22 +16,3 @@ CREATE TABLE "car"
         ON DELETE CASCADE
 );
 
-
---
--- CREATE TABLE car
--- (
---     car_Id integer NOT NULL AUTO_INCREMENT,
---     user_Id_Ovner integer,
---     model text,
--- FOREIGN KEY (user_Id_Ovner)
---         REFERENCES user (user_Id)
---         ON UPDATE NO ACTION
---         ON DELETE CASCADE
--- );
---
--- CREATE TABLE user
--- (
---     user_Id integer NOT NULL AUTO_INCREMENT,
---     name text,
---     PRIMARY KEY (user_Id)
--- );
